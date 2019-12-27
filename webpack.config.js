e41936544cc47be8eaf6d0ca68e01411b1ba84b9
fathiyahru <fathiyah.u@weefer.co.id>
2019-12-27 09:22:56 +0700

    Isi dengan keterangan untuk commit

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
                 enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
+                test: /\.tsx$/,
+                loader: ['babel-loader', 'awesome-typescript-loader'],
             }
         ]
     },
